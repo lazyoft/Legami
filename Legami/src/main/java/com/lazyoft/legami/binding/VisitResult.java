949ae613064eaf9a84c9c3b968cdf769cd160635
Fabrizio Chignoli <chignoli@pobox.com>
2014-06-02 02:14:41 +0200

    Added VisitResult and VisitResultList as result classes for the token visitor

---
+++ Legami/src/main/java/com/lazyoft/legami/binding/VisitResult.java
@@ -0,0 +1,11 @@
+package com.lazyoft.legami.binding;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class VisitResult {
+    public IProperty target;
+    public IProperty source;
+    public Object parameter;
+    public boolean converted;
+}
